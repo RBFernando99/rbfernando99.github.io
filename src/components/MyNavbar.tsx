202765758b889d9a01042ff8da801debd3df67d7
Fernando R. B <RBFernando99@gmail.com>
2022-12-21 00:20:03 +0100

    fix: Navbar is now responsive

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 
 import Container from "react-bootstrap/Container";
+
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function MyNavbar() {
   return (
     <div>
-      <Navbar bg="primary" variant="dark" fixed="top">
+      <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
         <Container>
           <Navbar.Brand href="#app">Home</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#profile">Profile</Nav.Link>
-            <Nav.Link href="#education">Education</Nav.Link>
-            <Nav.Link href="#awards">Awards</Nav.Link>
-            <Nav.Link href="#professional-experience">
-              Professional Experience
-            </Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact-form">Contact</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#profile">Profile</Nav.Link>
+              <Nav.Link href="#education">Education</Nav.Link>
+              <Nav.Link href="#awards">Awards</Nav.Link>
+              <Nav.Link href="#professional-experience">
+                Professional Experience
+              </Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact-form">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
